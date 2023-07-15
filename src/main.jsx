8f33c2019ea794dcbfeb5ae0edf43780ea9d736a
Amine Sekkiou <amine1132@hotmail.fr>
2023-07-15 20:15:25 +0200

    adress on top right delete

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,31 @@
 //  Dependencies
 // ===========================================================
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Router from './core/Router'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./core/Router";
 
 //  Middlewares
 // ===========================================================
 
-import { Provider } from 'react-redux'
-import { store } from './stores/store'
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
 //import './services/i18n'
-
 
 //  Styles
 // ===========================================================
 
-import './styles/Global.css'
-
+import "./styles/Global.css";
 
 //  Core
 // ===========================================================
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <React.Suspense fallback="...loading">
-          <Router />
-        </React.Suspense>
-      </Provider>
-    </React.StrictMode>
-  )
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <React.Suspense fallback="...loading">
+        <Router />
+      </React.Suspense>
+    </Provider>
+  </React.StrictMode>
+);
--- src/routes/RouteLogged.jsx
+++ src/routes/RouteLogged.jsx
@@ -3,44 +3,38 @@
 
 import React, { useState, useEffect } from "react";
 import { useNavigate, Outlet, useLocation } from "react-router-dom";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
 
 //  Components
 // ===========================================================
 
 import Modal from "#components/Modal/Modal";
 
-
 //  Route
 // ===========================================================
 
 export const RouteLogged = () => {
-
   // Hooks
   const navigate = useNavigate();
   const location = useLocation();
-  const user = useSelector(state => state.settings.user);
+  const user = useSelector((state) => state.settings.user);
 
   // User
   useEffect(() => {
-
     // User is not connected
-    if (!user?.address)
-      navigate("/home");
-
+    if (!user?.address) navigate("/home");
   }, [user]);
 
   return (
     <div className="w-screen h-screen overflow-y-auto">
       <Outlet
         context={{
-          user
+          user,
         }}
       />
-      <div className="absolute top-0 right-0 z-[200] p-6">
+      {/*<div className="absolute top-0 right-0 z-[200] p-6">
         <Modal />
-      </div>
+      </div>*/}
     </div>
-  )
+  );
 };
