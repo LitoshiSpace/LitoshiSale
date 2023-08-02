8abb852bbc6a78244a57aa3c76ac0a2ef7f8adc9
YassHarrach <yassine.harrach@etu.esisar.grenoble-inp.fr>
2023-08-02 23:04:58 +0200

    update mint to airdrop

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -61,7 +61,7 @@ export default function Modal({ onAddressChange }) {
   return (
     <>
       <button onClick={toggleModal} className="btn-modal">
-        Mint
+        Claim Airdrop
       </button>
 
       {modal && (
